a82da26351ad2f76af218b2841fcf7cf82022c10
ddcovery <antonio@abrevia.net>
2022-02-03 23:48:39 +0100

    * Brute force  review

--- solution00_brute_force.d
+++ solution00_brute_force.d
@@ -1,28 +1,24 @@
+/**
+  count(S,D) Count how many configurations of D dice sum S
+
+  Brute force version
+
+  Test is performed for 1 to 12 dice only
+
+  Run with unittest and invariants
+  $ dmd -run  solution00_brute_force.d -o-
+  Run in release mode
+  $ dmd -run  solution00_brute_force.d --release -O -o- -check=off
+
+  Author: Antonio Cabrera Perez (2022-02-03)
+*/
 module solution00_brute_force;
-
-///
-///  count(S,D)  Count how many configurations of D dice sum S
-///
-///  Brute force algorithm
-///  Test is performed for 2 to 14 dice only
-///
-///
-///  Author: Antonio Cabrera Perez (2022-02-03)
-
-double count(ulong s, ulong d)
-{
-  double count = 0;
-  foreach (sum; Sums(d))
-    if (sum == s)
-      count++;
-  return count;
-}
 
 void main()
 {
   import std.stdio : writefln;
 
-  for (auto dice = 2; dice <= 14; dice += 2)
+  foreach (dice; 1 .. 13)
   {
     auto sum = (dice + 6 * dice) / 2;
     double result = 0.;
@@ -31,49 +27,16 @@ void main()
   }
 }
 
-
-
-/// Generate the sums of all variations with repetition of 6 elements taken in groups of <diceCount> elements
-struct Sums
+/**
+ Count how many configurations of <d> dice sum <s>
+ */
+double count(ulong s, ulong d)
 {
-  ulong diceCount;
+  import std.algorithm : count, filter;
 
-  auto opApply(int delegate(ulong) loopFn)
-  {
-    // <vector> is treated as a base 6 number:  next variation is generated adding 1 to actual one
-    byte[] vector = new byte[diceCount];
-    ulong actualSum = 0;
-    do
-    {
-      loopFn(actualSum + diceCount);
-      actualSum = nextSum(vector);
-    }
-    while (actualSum != 0);
-
-    return 1;
-  }
-
-  private ulong nextSum(ref byte[] vector)
-  {
-    import std.algorithm : sum;
-
-    foreach (die; 0 .. vector.length)
-    {
-      vector[die] = (vector[die] + 1) % 6;
-      if (vector[die] != 0)
-        break;
-    }
-    return vector.sum();
-  }
-
-}
-
-auto measure(alias f)()
-{
-  import std.datetime.stopwatch : benchmark;
-
-  auto bm = benchmark!(f)(1);
-  return bm[0].total!"msecs";
+  return DiceSums(d).
+    filter!(sum => sum == s).
+    count;
 }
 
 unittest
@@ -85,6 +48,68 @@ unittest
 
   assert(count(1, 3) == 0.);
   assert(count(3, 3) == 1.);
+  assert(count(10, 3) == 27.);
   assert(count(18, 3) == 1.);
   assert(count(19, 3) == 0.);
+
+}
+
+/**
+ Range that generates the sums of all variations with repetitions of 6 values taken in groups of <diceCount>
+ */
+struct DiceSums
+{
+
+  this(ulong diceCount)
+  {
+    vector = new byte[diceCount];
+  }
+
+  bool empty() const @property
+  {
+    return endReached;
+  }
+
+  ulong front() const @property
+  {
+    return vectorSum + vector.length; // vector.length === diceCount 
+  }
+
+  void popFront()
+  {
+    import std.algorithm : sum;
+
+    // <vector> is treated as a base 6 number:  next variation is generated adding 1 to actual one  
+    foreach (die; 0 .. vector.length)
+    {
+      vector[die] = (vector[die] + 1) % 6;
+      if (vector[die] != 0)
+        break;
+    }
+    vectorSum = sum(vector);
+    endReached = vectorSum == 0;
+  }
+
+  private
+  {
+    byte[] vector;
+    ulong vectorSum = 0;
+    bool endReached = false;
+  }
+
+  invariant ()
+  {
+    import std.algorithm : sum;
+
+    assert(sum(vector) == vectorSum);
+    assert(!endReached || vectorSum == 0, "endReached -> vectorSum==0");
+  }
+}
+
+auto measure(alias f)()
+{
+  import std.datetime.stopwatch : benchmark;
+
+  auto bm = benchmark!(f)(1);
+  return bm[0].total!"msecs";
 }
--- solution00_brute_force.js
+++ solution00_brute_force.js
@@ -2,7 +2,7 @@
 ///  count(S,D)  Count how many configurations of D dice sum S
 ///
 ///  Brute force solution
-///  Test is performed for 2 to 12 dice only
+///  Test is performed for 1 to 12 dice only
 ///
 ///  Algorithm fails when the sum of a dice configuration exceeds the floating point precission
 ///
@@ -11,8 +11,8 @@
 main();
 
 function main() {
-  for (let dice = 2; dice <= 12; dice += 2) {
-    let sum = (dice + 6 * dice) / 2;
+  for (let dice = 1; dice <= 12; dice++) {
+    let sum = Math.floor((dice + 6 * dice) / 2);
     console.time("time");
     console.log({ sum, dice, result: count(sum, dice) });
     console.timeEnd("time");
@@ -21,33 +21,34 @@ function main() {
 }
 
 function count(s, d) {
-  let count = 0;
-  let vr = vr_generator(d)
-  do {
-    if (vr.sum() === s) count++;
-  } while (vr.next());
-  return count;
+  let count = 0
+  enumerateAllVariationsSums(d)(sum => {
+    if (sum === s) count++;
+  })
+  return count
 }
 
-// Generates variations with repetition of 6 elements taken in groups of <dice_count>.
-function vr_generator(total_dice) {
-  let vector = Array(total_dice).fill(0);
-  let sum = 0;
-  return {
-    // internal sum refers to values between 0 and 5, but 1 to 6 are expected externally: we sum 1 for each die.
-    sum: () => sum + total_dice,
-    next,
+
+function enumerateAllVariationsSums(total_dice) {
+  return (consumeFunc) => {
+    let diceVector = Array(total_dice).fill(0);
+    let lastSum = 0;
+    do {
+      consumeFunc(lastSum + total_dice);
+
+      setNextVariation(diceVector)
+      lastSum = diceVector.reduce((t, s) => t + s, 0);
+    } while (lastSum !== 0)
+
   }
-  function next() {
-    // <vector> is treated as a base 6 number:  next variation is generated adding 1 to the actual one
-    for (let die = 0; die < vector.length; die++) {
-      vector[die] = (vector[die] + 1) % 6;
-      if (vector[die]!==0) break;
+
+  function setNextVariation(diceVector) {
+    // <diceVector> is treated as a base 6 number:  next vector is generated adding 1 to the actual one
+    for (let die = 0; die < diceVector.length; die++) {
+      diceVector[die] = (diceVector[die] + 1) % 6;
+      if (diceVector[die] != 0)
+        break;
     }
-    sum = vector.reduce((total, side) => total + side, 0)
-    return sum !== 0; // All dice to 0
   }
 
 }
-
-
